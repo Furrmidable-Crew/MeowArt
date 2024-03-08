41cd716847508edae25e6164d4eca70a8b2069ab
Nicorb <67009524+nicola-corbellini@users.noreply.github.com>
2024-03-08 19:42:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ class Model(Enum):
     dalle2: str = 'dall-e-2'
 
 class Settings(BaseModel):
-    api_key: str = Field(title="API Key", description="The API key for OpenAI's image generation API.", default=""),
-    image_size: ImageSize = Field(title="Image size", description="The size for the image to generate. For dall-e-3 you can choose between '1024x1024', '1792x1024' and '1024x1792' while for dall-e-2 you can choose between '1024x1024', '512x512' and '256x256'", default=ImageSize.quad),
-    quality: Quality = Field(title="Image quality", description="The quality for the image to generate. It will be used only with dall-e-3", default=Quality.hd),
-    style: Style = Field(title="Image style", description="The style for the image to generate. It will be used only with dall-e-3", default=Style.natural),
+    api_key: str = Field(title="API Key", description="The API key for OpenAI's image generation API.", default="")
+    image_size: ImageSize = Field(title="Image size", description="The size for the image to generate. For dall-e-3 you can choose between '1024x1024', '1792x1024' and '1024x1792' while for dall-e-2 you can choose between '1024x1024', '512x512' and '256x256'", default=ImageSize.quad)
+    quality: Quality = Field(title="Image quality", description="The quality for the image to generate. It will be used only with dall-e-3", default=Quality.hd)
+    style: Style = Field(title="Image style", description="The style for the image to generate. It will be used only with dall-e-3", default=Style.natural)
     model: Model = Field(title="Model", description="The model to use for the image to generate", default=Model.dalle3)    
     
     @model_validator(mode='after')
